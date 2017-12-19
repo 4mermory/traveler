52e0429ac671aeca9c74b72f35ef4d0869c24d9c
mermory <2084170933@qq.com>
2017-12-19 19:40:44 +0800

    second commit file1

---
+++ traveler/src/main/java/com/travel/controller/TeamController.java
@@ -0,0 +1,15 @@
+package com.travel.controller;/**
+ * @Author:4mermory
+ * @Description:团队测试controller
+ * @Date:created in  2017-12-2017/12/19  19:38
+ * @Modified By:
+ */
+
+/**
+ * com.travel.controller
+ * 团队测试controller
+ * @author 4mermory
+ * @create 2017-12-19 19:38
+ **/
+public class TeamController {
+}
---
+++ traveler/src/main/java/com/travel/controller/TrafficController.java
@@ -0,0 +1,15 @@
+package com.travel.controller;/**
+ * @Author:4mermory
+ * @Description:交通Controller
+ * @Date:created in  2017-12-2017/12/19  19:39
+ * @Modified By:
+ */
+
+/**
+ * com.travel.controller
+ * 交通Controller
+ * @author 4mermory
+ * @create 2017-12-19 19:39
+ **/
+public class TrafficController {
+}
