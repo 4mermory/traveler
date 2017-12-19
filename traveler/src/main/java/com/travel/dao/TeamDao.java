955685c23c49cd4c0a56e3f1986c97b3ba0ca510
mermory <2084170933@qq.com>
2017-12-19 19:31:52 +0800

    second commit file

---
+++ traveler/src/main/java/com/travel/dao/TeamDao.java
@@ -0,0 +1,10 @@
+package com.travel.dao;
+
+/**
+ * @Author:4mermory
+ * @Description:${DESCRIPTION}
+ * @Date:created in  2017-12-2017/12/19  19:31
+ * @Modified By:
+ */
+public interface TeamDao {
+}
